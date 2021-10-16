63593c2d27cbccc96f94559361a743eaa09b4be5
Emmanuel-tuyisenge <tuyisenge.emmanuel@yahoo.fr>
2021-10-16 15:10:59 +0200

    Component

--- src/com/manu/springdemo/TennisCoach.java
+++ src/com/manu/springdemo/TennisCoach.java
@@ -2,7 +2,7 @@ package com.manu.springdemo;
 
 import org.springframework.stereotype.Component;
 
-@Component("thatSillyCoach")
+@Component
 public class TennisCoach implements Coach {
 
     @Override
